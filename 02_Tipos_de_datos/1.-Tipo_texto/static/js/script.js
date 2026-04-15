3426e93a46b80314de3a8e6fa05b8f99a74c5a5c
jeremiasgaete <jeremiasgaete@liceovvh.cl>
2026-04-15 08:12:38 -0400

    ye

---
+++ 02_Tipos_de_datos/1.-Tipo_texto/static/js/script.js
@@ -0,0 +1,10 @@
+console.log("conexión con js correcta")
+/*
+Dato: Tipo texto(string)
+1.- Concatenación de texto con el signo +
+podemos unir texto y variables
+*/
+const nombre = "Jeremías";
+const apellido = "Gaete";
+//Unimos ambas constantes con un texto extra.
+console.log("Hola, mi nombre es. " + nombre + " " + apellido);
